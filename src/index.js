07566ca4e2b0762eae16f1c833144afced110251
Sarcastyx <gupta.aashish@gmail.com>
2017-06-27 21:44:36 +1000

    Removed the comments from the files.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/app';
-// import registerServiceWorker from './registerServiceWorker';
 import './Styles/index.css';
 
 ReactDOM.render(<App />, document.getElementById('news-reader-app'));
-// registerServiceWorker();
